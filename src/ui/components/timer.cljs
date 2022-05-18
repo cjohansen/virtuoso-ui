58d8a00c050b192c690ab3d4f7fd7e28882cd8dc
Christian Johansen <christian@cjohansen.no>
2022-05-18 10:30:16 +0200

    Make the timer progress line reset without animation

--- src/ui/components/timer.cljs
+++ src/ui/components/timer.cljs
@@ -4,7 +4,19 @@
   (:require-macros [cljs.core.async.macros :refer [go]]))
 
 (d/defcomponent timer
-  :on-render (fn [el]
+  :on-render (fn [el {:keys [progress]}]
+               (let [line (.querySelector el ".timer-progress-line")]
+                 (if (= 0 progress)
+                   ;; The progress line should only animate as it "runs out". If
+                   ;; it's reset (to any value > 0), it should just reset
+                   ;; instantly.
+                   (set! line.style.transition "none")
+                   (when (re-find #"^none" line.style.transition)
+                     (go
+                       ;; Reset transition asynchronously to avoid the line
+                       ;; animating slowly from 0 to 100% width.
+                       (<! (timeout 100))
+                       (set! line.style.transition "width 1s linear")))))
                (let [separator (.querySelector el ".timer-separator")]
                  (go
                    (set! separator.style.transition "opacity 400ms")
