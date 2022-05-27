d66c9651c65e3a3fae0080d219483486629f7e30
Christian Johansen <christian@cjohansen.no>
2022-05-27 16:07:15 +0200

    Execute command on login

--- src/virtuoso/login_page/page.cljs
+++ src/virtuoso/login_page/page.cljs
@@ -1,19 +1,31 @@
 (ns virtuoso.login-page.page
-  (:require [virtuoso.login-page.components :refer [login-page-component]]))
+  (:require [virtuoso.login-page.components :refer [login-page-component]]
+            [clojure.string :as str]))
+
+(defn valid-email? [email]
+  (and (string? email)
+       (re-find #"^[^@]+@[^\.]+\.[^\.]+" (str/trim email))))
 
 (defn prepare-login-page [state location]
-  {:title [:i18n/k ::title]
-   :logo {:logo/kind :virtuoso/name
-          :url "/images/virtuoso-logo.svg"
-          :color "var(--silver-chalice)"
-          :theme :theme/blue-gradient}
-   :symbol {:logo/kind :virtuoso/gradient
-            :url "/images/virtuoso-greyscale.svg"}
-   :text [:i18n/k ::form-text]
-   :input {:placeholder [:i18n/k ::input-label]
-           :onInput [[:actions/assoc-in [:transient location :email] :event/target.value]]}
-   :button {:text [:i18n/k ::button]
-            :actions []}})
+  (let [email (get-in state [:transient location :email])
+        valid? (valid-email? email)]
+    {:title [:i18n/k ::title]
+     :logo {:logo/kind :virtuoso/name
+            :url "/images/virtuoso-logo.svg"
+            :color "var(--silver-chalice)"
+            :theme :theme/blue-gradient}
+     :symbol {:logo/kind :virtuoso/gradient
+              :url "/images/virtuoso-greyscale.svg"}
+     :text [:i18n/k ::form-text]
+     :input {:placeholder [:i18n/k ::input-label]
+             :onInput [[:actions/assoc-in [:transient location :email] :event/target.value]]}
+     :button {:text [:i18n/k ::button]
+              :disabled? (not valid?)
+              :actions (when valid?
+                         [[:actions/command :login/request-otp email]])}}))
+
+(defn register-actions [store event-bus]
+  )
 
 (def page
   {:location/route ["login"]
@@ -21,4 +33,5 @@
    :login-page? true
    :page-title ::page-title
    :prepare #'prepare-login-page
-   :component #'login-page-component})
+   :component #'login-page-component
+   :register-actions #'register-actions})
