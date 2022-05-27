9f194fc0ae3f157bff1c559ce283fcd566da5d2e
Christian Johansen <christian@cjohansen.no>
2022-05-27 08:05:14 +0200

    Format log output nicely

--- src/ui/logger.cljs
+++ src/ui/logger.cljs
@@ -1,5 +1,7 @@
 (ns ui.logger
-  (:require [taoensso.timbre :as log]))
+  (:require [cljs.pprint :as pprint]
+            [clojure.string :as str]
+            [taoensso.timbre :as log]))
 
 (defn configure-logging []
   (log/merge-config!
@@ -8,6 +10,21 @@
     {:console
      {:enabled? true
       :min-level :debug
+      :output-fn (fn [data]
+                   (str
+                    (->> (concat
+                          [(second (str/split (.toISOString (:instant data)) #"T"))
+                           (str/upper-case (name (:level data)))
+                           (str "[" (:?ns-str data) ":" (:?line data) "]")
+                           "-"]
+                          (take-while string? (:vargs data)))
+                         (str/join " "))
+                    (when-let [args (->> (:vargs data)
+                                         (drop-while string?)
+                                         (map (fn [d]
+                                                (with-out-str (pprint/pprint d))))
+                                         seq)]
+                      (str "\n" (str/join "\n" args)))))
       :fn #(if (:level %)
              (let [output-fn (:output-fn %)
                    formatted (output-fn %)
