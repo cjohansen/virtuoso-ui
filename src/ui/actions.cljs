91c64bd52ebd8bdbcd20356a2046686ce331d514
Christian Johansen <christian@cjohansen.no>
2022-05-28 23:19:29 +0200

    Expose action to navigate the app

--- src/ui/actions.cljs
+++ src/ui/actions.cljs
@@ -1,10 +1,12 @@
 (ns ui.actions
   (:require [ui.event-bus :as bus]
             [ui.misc :as misc]
+            [ui.navigator :as navigator]
             [ui.picard :as picard]))
 
-(defn register-actions [store event-bus]
+(defn register-actions [{:keys [store event-bus] :as app}]
   (let [subs (partial bus/subscribe event-bus ::action)]
     (subs :actions/assoc-in #(apply swap! store misc/assoc-in* %&))
     (subs :actions/dissoc-in #(apply swap! store misc/dissoc-in* %&))
-    (subs :actions/command #(picard/<command! store event-bus %&))))
+    (subs :actions/command #(picard/<command! store event-bus %&))
+    (subs :actions/go-to-location #(apply navigator/go-to-location app %&))))
